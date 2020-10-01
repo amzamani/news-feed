65d6ebcf568f04a660cfa915f9fae04db6a03ba2
amzamani <abuholyangels@gmail.com>
2020-10-01 17:49:06 +0530

    working protoype 1-oct-5:48pm
    Signed-off-by: amzamani <abuholyangels@gmail.com>

---
+++ newsfeed.js
@@ -0,0 +1,25 @@
+
+// let navlinks = $('.navbar-nav .nav-link')
+
+// navlinks.click((ev) => {
+
+//   let componentUrl = `/${$(ev.target).attr('data-component')}.html`
+//   $('#content').load(componentUrl)
+
+// })
+
+// let jresp;
+
+
+// async function getNews() {
+    
+//     const response = await fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=QJqLn6OcDEABCBC11kTCjoOmRCU0Jrma');
+//     const jsonResp = await response.json();
+//     jresp = jsonResp;
+//     console.log(jresp);
+//     return jsonResp;
+// }
+// getNews();
+// console.log(jresp);
+// var templateResponse = Handlebars.compile( $("#message-response-template").html());
+// console.log(templateResponse);
